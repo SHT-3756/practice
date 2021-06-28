177b26b00f94c0d21a4e3e8042790102932fff83
SHT-3756 <gkdlfndkekd@naver.com>
2021-06-28 15:40:17 +0900

    'useAsync.js에커스텀HOOK만들기'

--- react/api-integrate/src/Users.js
+++ react/api-integrate/src/Users.js
@@ -1,58 +1,24 @@
-import React, { useReducer, useEffect } from "react";
+import React from "react";
 import axios from "axios";
+import useAsync from "./useAsync";
 
-function reducer(state, action) {
-  switch (action.type) {
-    case "LOADING":
-      return {
-        loading: true,
-        data: null,
-        error: null,
-      };
-    case "SUCCESS":
-      return {
-        loading: false,
-        data: action.data,
-        error: null,
-      };
-    case "ERROR":
-      return {
-        loading: false,
-        data: null,
-        error: action.error,
-      };
+// useAsync 에서는 Promise 의 결과를 바로 data에 담기 때문에,
+// 요청한 이후에 response에서 data추출해 반환하는 함수 따로 만들었다.
 
-    default:
-      throw new Error(`action type : ${action.type}`);
-  }
+async function getUsers() {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+  return response.data;
 }
 function Users() {
-  const [state, dispatch] = useReducer(reducer, {
-    loading: false,
-    data: null,
-    error: null,
-  });
+  const [state, refetch] = useAsync(getUsers, [], true);
 
-  const fetchUsers = async () => {
-    dispatch({ type: "LOADING" });
-    try {
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/users"
-      );
-      dispatch({ type: "SUCCESS", data: response.data });
-    } catch (e) {
-      dispatch({ type: "ERROR", error: e });
-    }
-  };
+  const { loading, data: users, error } = state; //state.data를 users키워드로 조회
 
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
-  const { loading, data: users, error } = state;
   if (loading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다.</div>;
-  if (!users) return null;
+  if (!users) return <button onClick={refetch}>불러오기</button>;
   return (
     <>
       <ul>
@@ -62,7 +28,7 @@ function Users() {
           </li>
         ))}
       </ul>
-      <button onClick={fetchUsers}>다시불러오기</button>
+      <button onClick={refetch}>다시불러오기</button>
     </>
   );
 }

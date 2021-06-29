12752ab4a9f383a3256c22c1946ee29f77d94f1b
SHT-3756 <gkdlfndkekd@naver.com>
2021-06-29 19:59:07 +0900

    'react-router기능'

--- react/router-tutorial/src/Profiles.js
+++ react/router-tutorial/src/Profiles.js
@@ -1,17 +1,27 @@
 import React from "react";
-import { Link, Route } from "react-router-dom";
+import { NavLink, Route } from "react-router-dom";
 import Profile from "./Profile";
-
+import WithRouterSample from "./WithRouterSample";
 const Profiles = () => {
   return (
     <div>
       <h3>유저목록:</h3>
       <ul>
         <li>
-          <Link to="/profiles/first">first</Link>
+          <NavLink
+            to="/profiles/first"
+            activeStyle={{ background: "black", color: "white" }}
+          >
+            first
+          </NavLink>
         </li>
         <li>
-          <Link to="/profiles/second">second</Link>
+          <NavLink
+            to="/profiles/second"
+            activeStyle={{ background: "black", color: "white" }}
+          >
+            second
+          </NavLink>
         </li>
       </ul>
       <Route
@@ -20,6 +30,7 @@ const Profiles = () => {
         render={() => <div>유저를 선택해주세요.</div>}
       />
       <Route path="/profiles/:username" component={Profile} />
+      <WithRouterSample />
     </div>
   );
 };

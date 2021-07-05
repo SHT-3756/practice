82606a55e313dadaf64c0f5788eeae78cdd3d1b2
SHT-3756 <gkdlfndkekd@naver.com>
2021-07-05 17:18:02 +0900

    'counter.js로프리젠테이셔널컴포넌트,컨테이너컴포넌트알아보기'

--- react/learn-redux/src/index.js
+++ react/learn-redux/src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { createStore } from "redux";
 import rootReducer from "./modules";
-import { Provider } from "react--redux";
+import { Provider } from "react-redux";
 
 const store = createStore(rootReducer); //스토어를 만들어 준다.
 // console.log(store.getState()); //스토의 현재 상태를 확인한다.

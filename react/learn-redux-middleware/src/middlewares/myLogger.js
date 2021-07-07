cf1e492b0b3d0a18418b83c22ce55eb0250285b0
SHT-3756 <gkdlfndkekd@naver.com>
2021-07-07 16:40:12 +0900

    '미들웨어작성후적용'

---
+++ react/learn-redux-middleware/src/middlewares/myLogger.js
@@ -0,0 +1,6 @@
+const myLogger = (store) => (next) => (action) => {
+  console.log(action); // 먼저 액션을 출력한다.
+  const result = next(action); // 다음 미들웨어 (또는 리듀서) 에게 액션을 전달한다.
+  return result; // 여기서 리턴 값은 dispatch(action)의 결과값이 된다. 기본은 undefinded
+};
+export default myLogger;

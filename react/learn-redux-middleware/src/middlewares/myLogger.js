456b9936ac2912bbfc3dacb71063653c72bfb272
SHT-3756 <gkdlfndkekd@naver.com>
2021-07-07 17:18:10 +0900

    'redux-logger설치및적용,ReduxDevTools설치및적용'

--- react/learn-redux-middleware/src/middlewares/myLogger.js
+++ react/learn-redux-middleware/src/middlewares/myLogger.js
@@ -1,6 +1,9 @@
 const myLogger = (store) => (next) => (action) => {
   console.log(action); // 먼저 액션을 출력한다.
   const result = next(action); // 다음 미들웨어 (또는 리듀서) 에게 액션을 전달한다.
+
+  // 업데이트 이후의 상태를 조회한다.
+  console.log("\t", store.getState());
   return result; // 여기서 리턴 값은 dispatch(action)의 결과값이 된다. 기본은 undefinded
 };
 export default myLogger;

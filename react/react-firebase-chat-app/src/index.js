a5ba2cd4470e4fccff7d09337913d14408a977e5
SHT-3756 <gkdlfndkekd@naver.com>
2021-07-14 18:28:36 +0900

    '기존세팅및라이브러리설치'

---
+++ react/react-firebase-chat-app/src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import rootReducer from "./redux/reducer";
+
+const store = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider
+      store={store(
+        rootReducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+    >
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+reportWebVitals();

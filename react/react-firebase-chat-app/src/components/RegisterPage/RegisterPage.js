34843301f9e023b7fd3386abd3af69091e975cd6
SHT-3756 <gkdlfndkekd@naver.com>
2021-07-14 19:46:04 +0900

    ''회원가입,로그인UI

--- react/react-firebase-chat-app/src/components/RegisterPage/RegisterPage.js
+++ react/react-firebase-chat-app/src/components/RegisterPage/RegisterPage.js
@@ -1,7 +1,29 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function RegisterPage() {
-  return <div>RegisterPage</div>;
+  return (
+    <div className="auth-wrapper">
+      <div style={{ textAlign: "center" }}>
+        <h3>Register</h3>
+      </div>
+      <form>
+        <label>Email</label>
+        <input name="email" type="email" />
+        <label>Name</label>
+        <input name="name" />
+        <label>Password</label>
+        <input name="password" type="password" />
+
+        <label>PasswordConfirm</label>
+        <input name="password_confirm" type="password" />
+
+        <input type="submit" />
+        <Link to="/login" style={{ color: "gray", textDecoration: "none" }}>
+          이미 아이디가 있다면...
+        </Link>
+      </form>
+    </div>
+  );
 }
 
 export default RegisterPage;

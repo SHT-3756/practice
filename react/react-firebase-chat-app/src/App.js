1377eb6e8ef50fab529af34606c89102b7acffe3
SHT-3756 <gkdlfndkekd@naver.com>
2021-07-15 20:48:22 +0900

    'Redux스토어에로그인유저정보저장'

--- react/react-firebase-chat-app/src/App.js
+++ react/react-firebase-chat-app/src/App.js
@@ -6,9 +6,13 @@ import LoginPage from "./components/LoginPage/LoginPage";
 import RegisterPage from "./components/RegisterPage/RegisterPage";
 import { useEffect } from "react";
 import firebase from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { setUser } from "./redux/action/user_action";
 
 function App() {
   const history = useHistory();
+  const dispatch = useDispatch();
+  const isLoading = useSelector((state) => state.user.isLoading);
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
@@ -16,6 +20,7 @@ function App() {
       //로그인 된 상태
       if (user) {
         history.push("/");
+        dispatch(setUser(user));
       } else {
         //로그인 안된 상태
         history.push("/login");
@@ -23,13 +28,17 @@ function App() {
     });
   }, []);
 
-  return (
-    <Switch>
-      <Route exact path="/" component={ChatPage} />
-      <Route exact path="/login" component={LoginPage} />
-      <Route exact path="/register" component={RegisterPage} />
-    </Switch>
-  );
+  if (isLoading) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <Switch>
+        <Route exact path="/" component={ChatPage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/register" component={RegisterPage} />
+      </Switch>
+    );
+  }
 }
 
 export default App;

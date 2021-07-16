2eceb575c45d5347b7e766dfb9f22cea965fee76
SHT-3756 <gkdlfndkekd@naver.com>
2021-07-16 21:31:20 +0900

    'ChatPage추가'

--- react/react-firebase-chat-app/src/components/ChatPage/MainPanel/MainPanel.js
+++ react/react-firebase-chat-app/src/components/ChatPage/MainPanel/MainPanel.js
@@ -1,7 +1,28 @@
 import React from "react";
+import Message from "./Message";
+import MessageForm from "./MessageForm";
+import MessageHeader from "./MessageHeader";
 
 function MainPanel() {
-  return <div>MainPanel</div>;
+  return (
+    <div style={{ padding: "2rem 2rem 0 2rem" }}>
+      <MessageHeader />
+      <div
+        style={{
+          width: "100%",
+          height: "450px",
+          border: "0.2rem solid #ececec",
+          borderRadius: "4px",
+          padding: "1rem",
+          marginBottom: "1rem",
+          overflow: "auto",
+        }}
+      >
+        <Message />
+      </div>
+      <MessageForm />
+    </div>
+  );
 }
 
 export default MainPanel;
--- react/react-firebase-chat-app/src/components/ChatPage/SidePanel/SidePanel.js
+++ react/react-firebase-chat-app/src/components/ChatPage/SidePanel/SidePanel.js
@@ -1,7 +1,26 @@
 import React from "react";
+import UserPanel from "./UserPanel";
+import Favorited from "./Favorited";
+import DirectMessage from "./DirectMessage";
+import ChatRooms from "./ChatRooms";
 
 function SidePanel() {
-  return <div>SidePanel</div>;
+  return (
+    <div
+      style={{
+        background: "#7b83eb",
+        padding: "2rem",
+        minHeight: "100vh",
+        color: "white",
+        minWidth: "275px",
+      }}
+    >
+      <UserPanel />
+      <Favorited />
+      <ChatRooms />
+      <DirectMessage />
+    </div>
+  );
 }
 
 export default SidePanel;

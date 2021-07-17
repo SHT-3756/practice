549c56fc0febf2193094e9359664bcf7b42533d0
SHT-3756 <gkdlfndkekd@naver.com>
2021-07-17 17:26:30 +0900

    'UsePanel수정'

--- react/react-firebase-chat-app/src/components/ChatPage/SidePanel/UserPanel.js
+++ react/react-firebase-chat-app/src/components/ChatPage/SidePanel/UserPanel.js
@@ -1,7 +1,42 @@
 import React from "react";
+import { IoIosChatboxes } from "react-icons/io";
+import Dropdown from "react-bootstrap/Dropdown";
+import Image from "react-bootstrap/Image";
+import { useSelector } from "react-redux";
 
 function UserPanel() {
-  return <div>UserPanel</div>;
+  const user = useSelector((state) => state.user.currentUser);
+
+  return (
+    <div>
+      {/* logo */}
+      <div style={{ color: "white" }}>
+        <IoIosChatboxes />
+        Chat App
+      </div>
+      <div style={{ display: "flex", marginBottom: "1rem" }}>
+        <Image
+          src={user && user.photoURL}
+          style={{ width: "30px", height: "30px", marginTop: "3px" }}
+          roundedCircle
+        />
+        <Dropdown>
+          <Dropdown.Toggle
+            variant="success"
+            id="dropdown-basic"
+            style={{ background: "transparent", border: "0px" }}
+          >
+            {user.displayName}
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#">프로필 사진 변경</Dropdown.Item>
+            <Dropdown.Item href="#">로그아웃</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    </div>
+  );
 }
 
 export default UserPanel;

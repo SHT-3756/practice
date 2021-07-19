9069bae5f4eb02d5285e9ac785404b4934ab25b5
SHT-3756 <gkdlfndkekd@naver.com>
2021-07-19 16:39:25 +0900

    '채팅룸리스트UI'

--- react/react-firebase-chat-app/src/components/ChatPage/SidePanel/ChatRooms.js
+++ react/react-firebase-chat-app/src/components/ChatPage/SidePanel/ChatRooms.js
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaRegSmileWink } from "react-icons/fa";
+import { FaPlus } from "react-icons/fa";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 function ChatRooms() {
-  return <div>ChatRooms</div>;
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <div>
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <FaRegSmileWink style={{ marginRight: 3 }} />
+        CHAT ROOMS
+        <FaPlus
+          onClick={handleShow}
+          style={{ position: "absolute", right: 0, cursor: "pointer" }}
+        />
+      </div>
+      {/* modal */}
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>채팅방 만들기</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>방 이름</Form.Label>
+              <Form.Control type="text" placeholder="방 이름을 입력하세요." />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>방 설명</Form.Label>
+              <Form.Control type="text" placeholder="방 설명을 입력하세요." />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            닫기
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            방 생성
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 }
 
 export default ChatRooms;

f8abf6749eddca5873bc77a7557175d77dce92f5
SHT-3756 <gkdlfndkekd@naver.com>
2021-07-23 15:51:15 +0900

    'class형으로ChatRooms완성'

--- react/react-firebase-chat-app/src/components/ChatPage/SidePanel/ChatRooms.js
+++ react/react-firebase-chat-app/src/components/ChatPage/SidePanel/ChatRooms.js
@@ -1,173 +1,169 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import { FaRegSmileWink } from "react-icons/fa";
 import { FaPlus } from "react-icons/fa";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { useSelector } from "react-redux";
-import firebase from "firebase";
+import { connect } from "react-redux";
+import firebase from "../../../firebase";
 import { setCurrentChatRoom } from "../../../redux/action/chatRoom_action";
-import { useDispatch } from "react-redux";
+export class ChatRooms extends Component {
+  state = {
+    show: false,
+    name: "",
+    description: "",
+    chatRoomsRef: firebase.database().ref("chatRooms"),
+    chatRooms: [],
+    firstLoad: true,
+    activeChatRoomId: "",
+  };
+  componentDidMount() {
+    this.AddChatRoomsListeners();
+  }
+  setFirstChatRoom = () => {
+    const firstChatRoom = this.state.chatRooms[0];
+    if (this.state.firstLoad && this.state.chatRooms.length > 0) {
+      this.props.dispatch(setCurrentChatRoom(firstChatRoom));
+      this.setState({ activeChatRoomId: firstChatRoom.id });
+    }
+    this.setState({ firstLoad: false });
+  };
+  AddChatRoomsListeners = () => {
+    let chatRoomsArray = [];
 
-function ChatRooms() {
-  const [show, setShow] = useState(false);
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-
-  const chatRoomsRef = firebase.database().ref("chatRooms");
-  const [chatRooms, setChatRooms] = useState([]);
-  const dispatch = useDispatch();
-  // const [firstLoad, setFirstLoad] = useState(true);
-  const [activeChatRoomId, setActiveChatRoomId] = useState("");
-
-  // const setFirstChatRoom = () => {
-  //   const firstChatRoom = chatRooms[0];
-  //   if (firstLoad && chatRooms.length > 0) {
-  //     dispatch(setCurrentChatRoom(firstChatRoom));
-  //     setActiveChatRoomId(firstChatRoom.id);
-  //   }
-  //   setFirstLoad(false);
-  // };
-
-  const AddChatRoomsListeners = () => {
-    const chatRoomsArray = [];
-
-    chatRoomsRef.on("child_added", (DataSnapshot) => {
+    this.state.chatRoomsRef.on("child_added", (DataSnapshot) => {
       chatRoomsArray.push(DataSnapshot.val());
-      // console.log("chatRoomsArray", chatRoomsArray);
-      setChatRooms(chatRoomsArray);
 
-      // 함수형으로 이런식으로 해야하는데...
-      // this.setState({ chatRooms: chatRoomsArray }, () =>
-      //   this.setFirstChatRoom()
-      // );
+      this.setState({ chatRooms: chatRoomsArray }, () =>
+        this.setFirstChatRoom()
+      );
     });
   };
-  // useEffect(() => {
-  //   console.log("ChatRooms", chatRooms);
-  // }, [chatRooms]);
+  handleClose = () => this.setState({ show: false });
+  handleShow = () => this.setState({ show: true });
 
-  useEffect(() => {
-    AddChatRoomsListeners();
-    // console.log("chatRooms1", chatRooms);
-  }, []);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  //리덕스에 있는 유저정보를 담았다.
-  const user = useSelector((state) => state.user.currentUser);
-
-  // 저장 버튼, 버튼을 누르면 제목, 설명, 유저정보가 파이어베이스에 저장되어야한다.
-  const handleSubmit = (e) => {
-    // 새로고침 방지
+  handleSubmit = (e) => {
     e.preventDefault();
-    //제목
-    if (isFormValid(name, description)) {
-      addChatRoom();
+
+    const { name, description } = this.state;
+
+    if (this.isFormValid(name, description)) {
+      this.addChatRoom();
     }
   };
-  const isFormValid = (name, description) => name && description;
 
-  const changeChatRoom = (room) => {
-    dispatch(setCurrentChatRoom(room));
-    setActiveChatRoomId(room.id);
-  };
-
-  const renderChatRooms = (chatRooms) =>
-    chatRooms.length > 0 &&
-    chatRooms.map((room) => (
-      <li
-        key={room.id}
-        style={{ backgroundColor: room.id === activeChatRoomId && "#ffffff45" }}
-        onClick={() => changeChatRoom(room)}
-      >
-        #{room.name}
-      </li>
-    ));
-
-  const addChatRoom = async () => {
-    //채팅방을 추가하는 기능
-    const key = chatRoomsRef.push().key;
+  addChatRoom = async () => {
+    const key = this.state.chatRoomsRef.push().key;
+    const { name, description } = this.state;
+    const { user } = this.props;
     const newChatRoom = {
       id: key,
       name: name,
       description: description,
-      createBy: {
+      createdBy: {
         name: user.displayName,
         image: user.photoURL,
       },
     };
+
     try {
-      await chatRoomsRef
-        .child(key)
-        .update(newChatRoom)
-        .then(setName(""), setDescription(""), setShow(false));
+      await this.state.chatRoomsRef.child(key).update(newChatRoom);
+      this.setState({
+        name: "",
+        description: "",
+        show: false,
+      });
     } catch (error) {
       alert(error);
     }
   };
-
-  return (
-    <div>
-      <div
+  changeChatRoom = (room) => {
+    this.props.dispatch(setCurrentChatRoom(room));
+    this.setState({ activeChatRoomId: room.id });
+  };
+  isFormValid = (name, description) => name && description;
+  renderChatRooms = (chatRooms) =>
+    chatRooms.length > 0 &&
+    chatRooms.map((room) => (
+      <li
+        key={room.id}
         style={{
-          position: "relative",
-          width: "100%",
-          display: "flex",
-          alignItems: "center",
+          backgroundColor:
+            room.id === this.state.activeChatRoomId && "#ffffff45",
         }}
+        onClick={() => this.changeChatRoom(room)}
       >
-        <FaRegSmileWink style={{ marginRight: 3 }} />
-        CHAT ROOMS
-        <FaPlus
-          onClick={handleShow}
-          style={{ position: "absolute", right: 0, cursor: "pointer" }}
-        />
+        # {room.name}
+      </li>
+    ));
+  render() {
+    return (
+      <div>
+        <div
+          style={{
+            position: "relative",
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <FaRegSmileWink style={{ marginRight: 3 }} />
+          CHAT ROOMS
+          <FaPlus
+            onClick={this.handleShow}
+            style={{ position: "absolute", right: 0, cursor: "pointer" }}
+          />
+        </div>
+
+        <ul style={{ listStyleType: "none", padding: 0 }}>
+          {this.renderChatRooms(this.state.chatRooms)}
+        </ul>
+        {/* modal */}
+
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>채팅방 만들기</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>방 이름</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="방 이름을 입력하세요."
+                  onChange={(e) => this.setState({ name: e.target.value })}
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>방 설명</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="방 설명을 입력하세요."
+                  onChange={(e) =>
+                    this.setState({ description: e.target.value })
+                  }
+                />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              닫기
+            </Button>
+            <Button variant="primary" onClick={this.handleSubmit}>
+              방 생성
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
-
-      <ul style={{ listStyleType: "none", padding: 0 }}>
-        {renderChatRooms(chatRooms)}
-      </ul>
-
-      {/* modal */}
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>채팅방 만들기</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>방 이름</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="방 이름을 입력하세요."
-                onChange={(e) => setName(e.target.value)}
-              />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>방 설명</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="방 설명을 입력하세요."
-                onChange={(e) => setDescription(e.target.value)}
-              />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            닫기
-          </Button>
-          <Button variant="primary" onClick={handleSubmit}>
-            방 생성
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </div>
-  );
+    );
+  }
 }
 
-export default ChatRooms;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user.currentUser,
+  };
+};
+export default connect(mapStateToProps)(ChatRooms);
---
+++ react/react-firebase-chat-app/src/components/ChatPage/SidePanel/ChatRoomsFunctional.js
@@ -0,0 +1,173 @@
+import React, { useState, useEffect } from "react";
+import { FaRegSmileWink } from "react-icons/fa";
+import { FaPlus } from "react-icons/fa";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useSelector } from "react-redux";
+import firebase from "firebase";
+import { setCurrentChatRoom } from "../../../redux/action/chatRoom_action";
+import { useDispatch } from "react-redux";
+
+function ChatRooms() {
+  const [show, setShow] = useState(false);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
+  const chatRoomsRef = firebase.database().ref("chatRooms");
+  const [chatRooms, setChatRooms] = useState([]);
+  const dispatch = useDispatch();
+  // const [firstLoad, setFirstLoad] = useState(true);
+  const [activeChatRoomId, setActiveChatRoomId] = useState("");
+
+  // const setFirstChatRoom = () => {
+  //   const firstChatRoom = chatRooms[0];
+  //   if (firstLoad && chatRooms.length > 0) {
+  //     dispatch(setCurrentChatRoom(firstChatRoom));
+  //     setActiveChatRoomId(firstChatRoom.id);
+  //   }
+  //   setFirstLoad(false);
+  // };
+
+  const AddChatRoomsListeners = () => {
+    const chatRoomsArray = [];
+
+    chatRoomsRef.on("child_added", (DataSnapshot) => {
+      chatRoomsArray.push(DataSnapshot.val());
+      // console.log("chatRoomsArray", chatRoomsArray);
+      setChatRooms(chatRoomsArray);
+
+      // 함수형으로 이런식으로 해야하는데...
+      // this.setState({ chatRooms: chatRoomsArray }, () =>
+      //   this.setFirstChatRoom()
+      // );
+    });
+  };
+  // useEffect(() => {
+  //   console.log("ChatRooms", chatRooms);
+  // }, [chatRooms]);
+
+  useEffect(() => {
+    AddChatRoomsListeners();
+    // console.log("chatRooms1", chatRooms);
+  }, []);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  //리덕스에 있는 유저정보를 담았다.
+  const user = useSelector((state) => state.user.currentUser);
+
+  // 저장 버튼, 버튼을 누르면 제목, 설명, 유저정보가 파이어베이스에 저장되어야한다.
+  const handleSubmit = (e) => {
+    // 새로고침 방지
+    e.preventDefault();
+    //제목
+    if (isFormValid(name, description)) {
+      addChatRoom();
+    }
+  };
+  const isFormValid = (name, description) => name && description;
+
+  const changeChatRoom = (room) => {
+    dispatch(setCurrentChatRoom(room));
+    setActiveChatRoomId(room.id);
+  };
+
+  const renderChatRooms = (chatRooms) =>
+    chatRooms.length > 0 &&
+    chatRooms.map((room) => (
+      <li
+        key={room.id}
+        style={{ backgroundColor: room.id === activeChatRoomId && "#ffffff45" }}
+        onClick={() => changeChatRoom(room)}
+      >
+        #{room.name}
+      </li>
+    ));
+
+  const addChatRoom = async () => {
+    //채팅방을 추가하는 기능
+    const key = chatRoomsRef.push().key;
+    const newChatRoom = {
+      id: key,
+      name: name,
+      description: description,
+      createBy: {
+        name: user.displayName,
+        image: user.photoURL,
+      },
+    };
+    try {
+      await chatRoomsRef
+        .child(key)
+        .update(newChatRoom)
+        .then(setName(""), setDescription(""), setShow(false));
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  return (
+    <div>
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <FaRegSmileWink style={{ marginRight: 3 }} />
+        CHAT ROOMS
+        <FaPlus
+          onClick={handleShow}
+          style={{ position: "absolute", right: 0, cursor: "pointer" }}
+        />
+      </div>
+
+      <ul style={{ listStyleType: "none", padding: 0 }}>
+        {renderChatRooms(chatRooms)}
+      </ul>
+
+      {/* modal */}
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>채팅방 만들기</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>방 이름</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="방 이름을 입력하세요."
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>방 설명</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="방 설명을 입력하세요."
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            닫기
+          </Button>
+          <Button variant="primary" onClick={handleSubmit}>
+            방 생성
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
+
+export default ChatRooms;

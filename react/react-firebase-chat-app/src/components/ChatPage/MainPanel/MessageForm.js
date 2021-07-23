04899a318e2fdebeba606cc95360b08f4aebf0a9
SHT-3756 <gkdlfndkekd@naver.com>
2021-07-23 19:53:28 +0900

    '메세지Body,FormUI만들기'

--- react/react-firebase-chat-app/src/components/ChatPage/MainPanel/MessageForm.js
+++ react/react-firebase-chat-app/src/components/ChatPage/MainPanel/MessageForm.js
@@ -1,7 +1,32 @@
 import React from "react";
-
+import Form from "react-bootstrap/Form";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import { Row, Col } from "react-bootstrap";
 function MessageForm() {
-  return <div>MessageForm</div>;
+  return (
+    <div>
+      <Form>
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Control as="textarea" rows={2} />
+        </Form.Group>
+      </Form>
+
+      <ProgressBar variant="warning" label="60" now={60} />
+
+      <Row>
+        <Col>
+          <button className="message-form-button" style={{ width: "100%" }}>
+            SEND
+          </button>
+        </Col>
+        <Col>
+          <button className="message-form-button" style={{ width: "100%" }}>
+            UPLOAD
+          </button>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default MessageForm;

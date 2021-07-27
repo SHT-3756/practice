8be566e26f6c2b00da64023ee8df43fea520ad69
SHT-3756 <gkdlfndkekd@naver.com>
2021-07-27 21:02:30 +0900

    '메세지보여주기위한MainPanel클래스형추가'

--- react/react-firebase-chat-app/src/components/ChatPage/MainPanel/MainPanel.js
+++ react/react-firebase-chat-app/src/components/ChatPage/MainPanel/MainPanel.js
@@ -1,28 +1,65 @@
-import React from "react";
+import React, { Component } from "react";
+import MessageHeader from "./MessageHeader";
 import Message from "./Message";
 import MessageForm from "./MessageForm";
-import MessageHeader from "./MessageHeader";
+import { connect } from "react-redux";
+import firebase from "../../../firebase";
 
-function MainPanel() {
-  return (
-    <div style={{ padding: "2rem 2rem 0 2rem" }}>
-      <MessageHeader />
-      <div
-        style={{
-          width: "100%",
-          height: "450px",
-          border: "0.2rem solid #ececec",
-          borderRadius: "4px",
-          padding: "1rem",
-          marginBottom: "1rem",
-          overflow: "auto",
-        }}
-      >
-        <Message />
+export class MainPanel extends Component {
+  state = {
+    messages: [],
+    messagesRef: firebase.database().ref("messages"),
+    messagesLoading: true,
+  };
+  componentDidMount() {
+    const { chatRoom } = this.props;
+    if (chatRoom) {
+      this.addMessagesListeners(chatRoom.id);
+    }
+  }
+
+  addMessagesListeners = (chatRoomId) => {
+    let messageArray = [];
+    this.state.messagesRef
+      .child(chatRoomId)
+      .on("child_added", (DataSnapshot) => {
+        messageArray.push(DataSnapshot.val());
+        this.setState({ messages: messageArray, messagesLoading: false });
+      });
+  };
+
+  renderMessage = (messages) =>
+    messages.length > 0 &&
+    messages.map((message) => (
+      <Message
+        key={message.timestamp}
+        message={message}
+        user={this.props.user}
+      />
+    ));
+  render() {
+    const { messages } = this.state;
+    return (
+      <div style={{ padding: "2rem 2rem 0 2rem" }}>
+        <MessageHeader />
+        <div
+          style={{
+            width: "100%",
+            height: "450px",
+            border: "0.2rem solid #ececec",
+            borderRadius: "4px",
+            padding: "1rem",
+            marginBottom: "1rem",
+            overflow: "auto",
+          }}
+        >
+          {this.renderMessage(messages)}
+          <Message />
+        </div>
+        <MessageForm />
       </div>
-      <MessageForm />
-    </div>
-  );
+    );
+  }
 }
 
 export default MainPanel;

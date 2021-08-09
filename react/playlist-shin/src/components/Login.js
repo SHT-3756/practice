64ab7decea0cdb0bcd73dfcf27fe09e17e3c36cc
SHT-3756 <gkdlfndkekd@naver.com>
2021-08-09 18:53:16 +0900

    'Login페이지변경'

--- react/playlist-shin/src/components/Login.js
+++ react/playlist-shin/src/components/Login.js
@@ -1,13 +1,19 @@
 import React from "react";
+import "./Login.css";
 const AUTH_URL =
   "https://accounts.spotify.com/authorize?client_id=9381282f5a44490a81e1c96e2c051017&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 export default function Login() {
   return (
-    <div>
-      <h1>로그인을 먼저해야합니다.</h1>
-      <a className="btn" href={AUTH_URL}>
-        로그인
-      </a>
+    <div className="login">
+      <div class="drops">
+        <div class="drop" />
+        <div class="drop" />
+        <div class="drop" />
+        <div class="drop" />
+        <a className="btn" href={AUTH_URL}>
+          로그인
+        </a>
+      </div>
     </div>
   );
 }

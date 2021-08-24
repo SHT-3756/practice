d26eb1d44da4ad1046c379e51d302d9f1cb9bae6
SHT-3756 <gkdlfndkekd@naver.com>
2021-08-24 19:26:32 +0900

    'Modal_Css수정완료'

--- react/coin/src/components/Modal/ModalFunction.js
+++ react/coin/src/components/Modal/ModalFunction.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
 import { BsFillEyeFill } from "react-icons/bs";
+import "./Modal.css";
 
 export const ModalFunction = ({ coin }) => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
@@ -17,7 +18,7 @@ export const ModalFunction = ({ coin }) => {
       left: "50%",
       right: "auto",
       bottom: "auto",
-      width: "50%",
+      width: "60%",
       height: "70%",
       marginRight: "-50%",
       transform: "translate(-50%, -50%)",
@@ -36,9 +37,126 @@ export const ModalFunction = ({ coin }) => {
         onRequestClose={() => setModalIsOpen(false)}
         style={customStyles}
       >
-        <div onClick={setModalIsOpenToFalse}>
-          <img src={`${coin.coin.image}`} alt={`${coin.coin.name}`} />
-          <div> {coin.coin.name}</div>
+        <div className="ModalContent" onClick={setModalIsOpenToFalse}>
+          <div className="img">
+            <img src={`${coin.coin.image}`} alt={`${coin.coin.name}`} />
+          </div>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="cols">{coin.coin.name}</th>
+                <th scope="cols">내용</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">ath</th>
+                <td>{coin.coin.ath}</td>
+              </tr>
+              <tr>
+                <th scope="row">ath_change_percentage</th>
+                <td>{coin.coin.ath_change_percentage}</td>
+              </tr>
+              <tr>
+                <th scope="row">ath_date</th>
+                <td>{coin.coin.ath_date}</td>
+              </tr>
+              <tr>
+                <th scope="row">atl</th>
+                <td>{coin.coin.atl}</td>
+              </tr>
+              <tr>
+                <th scope="row">atl_change_percentage</th>
+                <td>{coin.coin.atl_change_percentage}</td>
+              </tr>
+              <tr>
+                <th scope="row">atl_date</th>
+                <td>{coin.coin.atl_date}</td>
+              </tr>
+              <tr>
+                <th scope="row">circulating_supply</th>
+                <td>{coin.coin.circulating_supply}</td>
+              </tr>
+              <tr>
+                <th scope="row">current_price</th>
+                <td>{coin.coin.current_price}</td>
+              </tr>
+              <tr>
+                <th scope="row">fully_diluted_valuation</th>
+                <td>
+                  {coin.coin.fully_diluted_valuation
+                    ? coin.coin.fully_diluted_valuation
+                    : "null"}
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">high_24h</th>
+                <td>{coin.coin.high_24h}</td>
+              </tr>
+              <tr>
+                <th scope="row">low_24h</th>
+                <td>{coin.coin.low_24h}</td>
+              </tr>
+              <tr>
+                <th scope="row">last_updated</th>
+                <td>{coin.coin.last_updated}</td>
+              </tr>
+              <tr>
+                <th scope="row">market_cap</th>
+                <td>{coin.coin.market_cap}</td>
+              </tr>
+              <tr>
+                <th scope="row">market_cap_change_24h</th>
+                <td>{coin.coin.market_cap_change_24h}</td>
+              </tr>
+              <tr>
+                <th scope="row">market_cap_change_percentage_24h</th>
+                <td>{coin.coin.market_cap_change_percentage_24h}</td>
+              </tr>
+              <tr>
+                <th scope="row">market_cap_rank</th>
+                <td>{coin.coin.market_cap_rank}</td>
+              </tr>
+              <tr>
+                <th scope="row">max_supply</th>
+                <td>{coin.coin.max_supply ? coin.coin.max_supply : "null"}</td>
+              </tr>
+              <tr>
+                <th scope="row">price_change_24h</th>
+                <td>{coin.coin.price_change_24h}</td>
+              </tr>
+              <tr>
+                <th scope="row">price_change_percentage_24h</th>
+                <td>{coin.coin.price_change_percentage_24h}</td>
+              </tr>
+              <tr>
+                <th scope="row">currency</th>
+                <td>{coin.coin.currency ? coin.coin.currency : "null"}</td>
+              </tr>
+              <tr>
+                <th scope="row">percentage</th>
+                <td>{coin.coin.percentage ? coin.coin.percentage : "null"}</td>
+              </tr>
+              <tr>
+                <th scope="row">times</th>
+                <td>{coin.coin.times ? coin.coin.times : "null"}</td>
+              </tr>
+              <tr>
+                <th scope="row">symbol</th>
+                <td>{coin.coin.symbol}</td>
+              </tr>
+              <tr>
+                <th scope="row">total_supply</th>
+                <td>
+                  {coin.coin.total_supply ? coin.coin.total_supply : "null"}
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">total_volume</th>
+                <td>{coin.coin.total_volume}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </Modal>
     </>
